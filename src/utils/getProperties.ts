93c99efef64282ac88c747dee7681119c7be4b89
S2kael <laiducminh1002@gmail.com>
2024-06-05 11:06:17 +0700

    Fix error when parsing chain props

--- src/utils/getProperties.ts
+++ src/utils/getProperties.ts
@@ -4,17 +4,17 @@ import { ChainError } from './errors'
 
 export async function getProperties(api: ApiPromise): Promise<ChainProps> {
   const props = await api.rpc.system.properties()
-  const base58Prefix = props.ss58Format.unwrapOr(null)?.toNumber()
+  const base58Prefix = props.ss58Format.unwrapOr(null)?.toNumber() ?? api.consts.system.ss58Prefix.toPrimitive() as number;
   const decimals = props.tokenDecimals.unwrapOr(null)?.[0].toNumber()
   const tokenSymbol = props.tokenSymbol.unwrapOr(null)?.[0].toString()
   const specName = api.runtimeVersion.specName.toString()
   const specVersion = api.runtimeVersion.specVersion.toNumber()
 
-  if (!base58Prefix) {
+  if (base58Prefix === undefined) {
     throw new ChainError('the base58 prefix was not found for this chain')
   }
 
-  if (!decimals) {
+  if (decimals  === undefined) {
     throw new ChainError('the chain decimals were not found for this chain')
   }
 
